aab510833ea1ad714449eb8882a17e26462aae36
Bello aisha <belloaishaaderonke@gmail.com>
2022-11-14 18:05:39 +0100

    button-correction

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -3,11 +3,13 @@ import Wallet from '../../Assets/Images/wallet.svg';
 
 const Button = () => {
     return(
-        <div className='border-header-100 '>
-            <button className='flex items-center bg-header-100 py-3.5 px-6 rounded-tl-lg rounded-br-lg '>
-                <span className='text-header-400 mr-2 font-circular font-medium'>Connect Wallet</span>
-                <img src={Wallet} alt="wallet" />
-            </button>  
+        <div>
+            <div className='border-[2px] border-header-100  rounded-tl-lg rounded-br-lg mt-[10px]'>
+                <button className='flex items-center bg-header-100 py-3.5 px-6 rounded-tl-lg rounded-br-lg ml-[5px] mb-[5px]'>
+                    <span className='text-header-400 mr-2 font-circular font-medium'>Connect Wallet</span>
+                    <img src={Wallet} alt="wallet" />
+                </button>  
+            </div>
         </div>
     )
 }
