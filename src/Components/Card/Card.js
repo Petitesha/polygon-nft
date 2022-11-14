d5b45fcc6f5ee9b84a2ffb652455a849875ceaa1
Bello aisha <belloaishaaderonke@gmail.com>
2022-11-14 17:51:21 +0100

    card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,7 +1,26 @@
+import Flowery from '../../Assets/Images/flowery knight.png';
+import Menu from '../../Assets/Images/dot menu.svg';
+import Etherum from '../../Assets/Images/etherum logo.svg';
+
 const Card = () => {
     return(
-        <div>
-            <p>This is the card section</p>
+        <div className='w-[261px] border-[1px] border-header-300'>
+           <div className='w-5/6 m-auto flex flex-col items-center justify-center my-3'>
+                <div className='mb-3'>
+                    <img src={Flowery} alt='Flowery' />
+                </div>
+                <div className='w-full'>
+                    <div className='flex items-center justify-between mb-2'>
+                        <p>Flowery knight</p>
+                        <img src={Menu} alt='menu' />
+                    </div>
+                    <p className='mb-2'>olajidebabbs</p>
+                    <div className='flex items-center justify-between mb-2'>
+                        <p>Etherum logo</p>
+                        <img src={Etherum} alt='etherum' />
+                    </div>
+                </div>
+           </div>
         </div>
     )
 }
