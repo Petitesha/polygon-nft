22cea1f06f90a8d8d4ddd36067627f3f44752713
Aisha <belloaishaaderonke@gmail.com>
2023-04-22 02:47:42 -0700

    bug fixes

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -6,7 +6,7 @@ const Button = () => {
         <div>
             <div className='border-[2px] border-header-100  rounded-tl-lg rounded-br-lg mt-[20px]'>
                 <button className='flex items-center bg-header-100 py-3.5 px-6 rounded-tl-lg rounded-br-lg ml-[5px] mb-[5px]'>
-                    <span className='text-header-400 mr-2 font-circular font-medium'>Connect Wallet</span>
+                    <span className='text-header-400 mr-2 font-circular font-medium'>Connect </span>
                     <img src={Wallet} alt="wallet" />
                 </button>  
             </div>
